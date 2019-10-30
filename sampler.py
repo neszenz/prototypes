6aaaec55121af82cb140ba2f3b7c22d3a90050a0
Tobias Newe <tobias.newe@t-online.de>
2019-10-30 17:28:46 +0100

    sampler w/ SIMPLE method + viewer implemented

--- sampler.py
+++ sampler.py
@@ -20,9 +20,10 @@ import datetime
 import os
 import random #TODO remove
 
+from OCC.Core.BRepAdaptor import BRepAdaptor_Curve
 from OCC.Core.Quantity import Quantity_Color, Quantity_TOC_RGB #TODO remove
 from OCC.Core.ShapeAnalysis import shapeanalysis
-from OCC.Core.TopAbs import TopAbs_FACE, TopAbs_WIRE, TopAbs_EDGE
+from OCC.Core.TopAbs import TopAbs_FACE, TopAbs_WIRE, TopAbs_EDGE, TopAbs_REVERSED
 from OCC.Core.TopExp import TopExp_Explorer, topexp
 from OCC.Core.TopTools import TopTools_IndexedMapOfShape
 from OCC.Display.SimpleGui import init_display #TODO remove
@@ -32,7 +33,7 @@ from OCC.Extend.TopologyUtils import TopologyExplorer
 
 PATH_BOX = '../resources/step/boxWithHole.step' #TODO remove
 PATH_42 = '../resources/abc/step/00090042/00090042_1cbd5c59fb8c0895c3599d4d_step_007.step' #TODO remove
-PATH = PATH_BOX #TODO remove
+INPUT_PATH = PATH_42 #TODO remove
 
 class SAMPLER_TYPE: # enum for calling sampler factory
     SIMPLE = 0
@@ -43,23 +44,41 @@ class SAMPLER_TYPE: # enum for calling sampler factory
     }
 
 OUTPUT_DIR = os.path.join('tmp', 'sampler')
+LINES_EXTENSION = '.lines'
 # to make written file names unique, a timestamp prefix is used
 TIMESTAMP = datetime.datetime.now()
-PREFIX_SHORT = TIMESTAMP.strftime('%y%b%d_%H%M%S_')
-PREFIX_LONG = TIMESTAMP.strftime('%y%b%d_%H%M%S_%f_')
+PREFIX_SHORT = TIMESTAMP.strftime('%y%b%d_%H%M%S')
+PREFIX_LONG = TIMESTAMP.strftime('%y%b%d_%H%M%S_%f')
+NUMBER_OF_SAMPLES = 100
 
 ## function body = + = + = + = + = + = + = + = + = + = + = + = + = + = + = + = +
 
 def generate_mesh_from_edge(edge, shape_maps):
     edge_mesh = list()
-    edge_mesh += [42]
+    curve = BRepAdaptor_Curve(edge)
+    fp = curve.FirstParameter()
+    lp = curve.LastParameter()
+    p_length = lp - fp
+    assert NUMBER_OF_SAMPLES > 1
+    for i in range(0, NUMBER_OF_SAMPLES):
+        parameter = fp + i * (p_length / (NUMBER_OF_SAMPLES-1))
+        if i == NUMBER_OF_SAMPLES-1:
+            parameter = lp
+        point = curve.Value(parameter)
+        edge_mesh.append([point.X(), point.Y(), point.Z()])
     return edge_mesh
 
-def generate_mesh_from_wire(wire, shape_maps):
+def generate_mesh_from_wire(wire, face_ori, shape_maps):
     wire_mesh = list()
     ex = TopExp_Explorer(wire, TopAbs_EDGE)
     while ex.More():
-        wire_mesh += generate_mesh_from_edge(ex.Current(), shape_maps)
+        edge = ex.Current()
+        edge_mesh = generate_mesh_from_edge(edge, shape_maps)
+        if len(edge_mesh) > 0:
+            if edge.Orientation() != face_ori:
+                edge_mesh.reverse()
+            edge_mesh.pop()
+            wire_mesh += edge_mesh
         ex.Next()
     return wire_mesh
 
@@ -67,15 +86,16 @@ def generate_mesh_from_face(face, shape_maps):
     _, wire_map, _ = shape_maps
     face_mesh = (list(), list())
     outer_loop, inner_loops = face_mesh
+    face_ori = face.Orientation()
     ex = TopExp_Explorer(face, TopAbs_WIRE)
     outer_wire_id = wire_map.FindIndex(shapeanalysis.OuterWire(face))
     while ex.More():
         wire = ex.Current()
         wire_id = wire_map.FindIndex(wire)
         if wire_id == outer_wire_id:
-            outer_loop += generate_mesh_from_wire(wire, shape_maps)
+            outer_loop += generate_mesh_from_wire(wire, face_ori, shape_maps)
         else:
-            inner_loops.append(generate_mesh_from_wire(wire, shape_maps))
+            inner_loops.append(generate_mesh_from_wire(wire, face_ori, shape_maps))
         ex.Next()
 
     return face_mesh
@@ -110,11 +130,34 @@ def factory(type):
         return generate_mesh_from_compound(compound)
     return sampler
 
-def write_mesh_to_file(mesh, name):
-    print('>> write to file \"', name, '\"', sep='')
-    print('mesh:', mesh)
+def generate_output_file_path():
+    name = PREFIX_SHORT + LINES_EXTENSION
+    path = os.path.join(OUTPUT_DIR, name)
+    if os.path.exists(path):
+        name = PREFIX_LONG + LINES_EXTENSION
+    assert not os.path.exists(path)
+    return path
+
+def write_mesh_to_file(mesh, path):
+    print('>> write to file \"', path, '\"', sep='')
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    def write_wire_mesh(wire_mesh, output):
+        for iVertex in range(0, len(wire_mesh)):
+            v0 = wire_mesh[iVertex]
+            v1 = wire_mesh[(iVertex + 1) % len(wire_mesh)]
+            output.write('%s %s %s' % (v0[0], v0[1], v0[2]))
+            output.write(' ')
+            output.write('%s %s %s' % (v1[0], v1[1], v1[2]))
+            output.write('\n')
+    with open(path, 'a') as output:
+        for face_mesh in mesh:
+            outer_wire_mesh = face_mesh[0]
+            write_wire_mesh(outer_wire_mesh, output)
+            for inner_wire_mesh in face_mesh[1]:
+                write_wire_mesh(inner_wire_mesh, output)
 
 if __name__ == '__main__':
     sampler = factory(SAMPLER_TYPE.SIMPLE)
-    mesh = sampler(PATH)
-    write_mesh_to_file(mesh, '')
+    mesh = sampler(INPUT_PATH)
+    output_path = generate_output_file_path()
+    write_mesh_to_file(mesh, output_path)
---
+++ viewer_lines.py
@@ -0,0 +1,94 @@
+import os
+import glob
+
+import pyglet
+from pyglet.gl import *
+from pyglet.window import key
+from pyglet.window import mouse
+
+INPUT_DIR = 'tmp'
+
+DEFAULT_X = 1280
+DEFAULT_Y = 720
+zoom = 25
+ZOOM_STEP = 5
+rotation = 0
+ROTATION_STEP = 10
+
+window = pyglet.window.Window(DEFAULT_X, DEFAULT_Y,
+                              fullscreen=False,
+                              resizable=True,
+                              vsync=True)
+window_caption = 'Delaunay Surface Mesh Refinement'
+
+
+def get_path_to_latest_lines_file():
+    files = sorted(glob.glob(os.path.join(INPUT_DIR, '**/*.lines')))
+    return files.pop()
+
+# lines are a tuple of two points, which are lists of coordinate values
+# returns a list of those lines
+def load_lines_form_file():
+    lines = []
+    path = get_path_to_latest_lines_file()
+    with open(path, 'r') as f:
+        for l in f:
+            fl = [float(i) for i in l.split()]
+            line = (fl[0:3], fl[3:])
+            lines.append(line)
+    return lines
+
+def updateProjectionMatrix(width, height):
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    global zoom
+    w = zoom
+    h = w / (width/height)
+    glOrtho(-w/2, w/2, -h/2, h/2, -w, w)
+    glMatrixMode(GL_MODELVIEW)
+
+@window.event
+def on_resize(width, height):
+    updateProjectionMatrix(width, height)
+    return pyglet.event.EVENT_HANDLED
+
+def apply_to_zoom(value):
+    global zoom
+    if zoom + value < 1:
+        zoom = 1
+    else:
+        zoom += value
+    updateProjectionMatrix(window.width, window.height)
+
+@window.event
+def on_key_press(symbol, modifiers):
+    global zoom, rotation
+    if symbol == key.UP:
+        apply_to_zoom(-ZOOM_STEP)
+    if symbol == key.DOWN:
+        apply_to_zoom(ZOOM_STEP)
+    if symbol == key.LEFT:
+        rotation -= ROTATION_STEP
+    if symbol == key.RIGHT:
+        rotation += ROTATION_STEP
+    # return pyglet.event.EVENT_HANDLED # disables ESC termination handler
+
+@window.event
+def on_draw():
+    lines = load_lines_form_file()
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+    global rotation
+    glRotatef(10.0, 1.0, 0.0, 0.0)
+    glRotatef(rotation, 0.0, 1.0, 0.0)
+    glBegin(GL_LINES)
+    for line in lines:
+        x0, y0, z0 = line[0]
+        x1, y1, z1 = line[1]
+        glVertex3f(x0, y0, z0)
+        glVertex3f(x1, y1, z1)
+    glEnd()
+
+if __name__ == '__main__':
+    pyglet.app.run()
