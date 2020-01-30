1f683cca08dc9300c85035e8c28657ccdde7c6eb
Tobias Newe <tobias.newe@t-online.de>
2020-01-30 20:09:13 +0100

    evaluator: added radius-edge ratio metric from DMG

--- evaluator.py
+++ evaluator.py
@@ -4,10 +4,12 @@ from util import *
 # metric configuration
 METRIC_ANGLES_HIST = False
 METRIX_XI_HIST = False
+METRIX_RATIO_HIST = False
 
 # number of bins for metric histograms
 METRIC_ANGLES_NOB = 53
 METRIX_XI_NOB = 32
+METRIX_RATIO_NOB = METRIX_XI_NOB
 
 # __main__ config
 INPUT_PATH = '../results/tmp/200128_190446.meshkD'
@@ -147,6 +149,62 @@ def normalized_area_metric(mesh):
             upper_bound = np.sqrt(3) / 4
             plot_histogram(xi_list, METRIX_XI_NOB, lower_bound, upper_bound)
 
+    return
+
+def radius_edge_ratio(mesh):
+    def calculate_ratio_for_all_triangles(vertices, triangles):
+        def calculate_ratio_for_triangle(p0, p1, p2):
+            # based on https://artofproblemsolving.com/wiki/index.php/Circumradius
+            l01 = np.linalg.norm(p1 - p0)
+            l12 = np.linalg.norm(p2 - p1)
+            l20 = np.linalg.norm(p0 - p2)
+
+            p01 = p1 - p0
+            p02 = p2 - p0
+            double_area = np.linalg.norm(np.cross(p01, p02))
+            assert double_area > 0.0
+
+            circumradius = (l01*l12*l20) / (2*double_area)
+            l_min = min(l01, l12, l20)
+            assert l_min > 0.0
+
+            return circumradius / l_min
+        ratio_list = []
+        min_ratio = float('inf')
+        max_ratio = float('-inf')
+
+        for (i0, i1, i2) in triangles:
+            sv0 = vertices[i0]
+            sv1 = vertices[i1]
+            sv2 = vertices[i2]
+
+            ratio = calculate_ratio_for_triangle(sv0.XYZ_vec3(), sv1.XYZ_vec3(), sv2.XYZ_vec3())
+            if ratio < min_ratio:
+                min_ratio = ratio
+            if ratio > max_ratio:
+                max_ratio = ratio
+
+            ratio_list.append(ratio)
+
+        return ratio_list, min_ratio, max_ratio
+    print('\n -- radius-edge ratio --')
+
+    num_of_faces = mesh.number_of_faces()
+    for i_face in range(mesh.number_of_faces()):
+        vertices, _, triangles, _ = mesh.face_meshes[i_face]
+        print('face_mesh ', i_face+1, ' of ', num_of_faces, ':', sep='')
+
+        ratio_list, min_ratio, max_ratio = calculate_ratio_for_all_triangles(vertices, triangles)
+        print(INDENT + 'range of ratio: ', end='')
+        print('[', round(min_ratio, 2), ', ', round(max_ratio, 2), ']', sep='')
+
+        if METRIX_RATIO_HIST:
+            lower_bound = 0.0
+            upper_bound = max(ratio_list) #TODO does finite upper bound exist?
+            plot_histogram(ratio_list, METRIX_RATIO_NOB, lower_bound, upper_bound)
+
+    return
+
 def evaluate(path):
     print('>> evaluating file', path)
     mesh = load_from_file(path)
@@ -157,7 +215,10 @@ def evaluate(path):
 
     normalized_area_metric(mesh)
 
+    radius_edge_ratio(mesh)
+
     print('')
+    return
 
 if __name__ == '__main__':
     evaluate(INPUT_PATH)
